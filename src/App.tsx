cae956b35f19e9163ec6aa3ca63af100f229b400
Pavel Novik <brn2befree@gmail.com>
2023-09-26 15:25:59 +0300

    finish dop-todolist-ex-3-dop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+
+type PropsType =
+    {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean
+    }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<Array<PropsType>>([])
+
+    const axiosRequest = () => {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then((res) => {
+                setTodos(res.data)
+            })
+    }
+
+    // axios.get('https://jsonplaceholder.typicode.com/todos')
+    //     .then((res) => {
+    //         setTodos(res.data)
+    //     })
+
+    useEffect(() => {
+        axiosRequest()
+    }, []);
+
+    // useEffect(() => {
+    //     axios.get('https://jsonplaceholder.typicode.com/todos')
+    //         .then((res) => {
+    //             setTodos(res.data)
+    //         })
+    // }, []);
+
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/todos')
+    //         .then(response => response.json())
+    //         .then(json => setTodos(json))
+    // }, [])
+
+    const onClickHandler = () => {
+        setTodos([])
+    }
+    const redisplayDataHandler = () => {
+        axiosRequest()
+    }
+
+    const mapTodos = todos.map(el => {
+        return (
+            <li key={el.id}>
+                <span>{el.id} - </span>
+                <span>{el.title}</span>
+                <span>{el.completed}</span>
+            </li>
+        )
+    })
+    return (
+        <div className="App">
+            <button onClick={onClickHandler}>CLEAN POSTS</button>
+            <button onClick={redisplayDataHandler}>Redisplay Data</button>
+            <ul>
+                {mapTodos}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
+
